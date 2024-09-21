55ca14c83ff6454540da617425c5a7ffcf8f0b04
Harshith1629 <harshith@purecodesoftware.com>
2024-09-21 10:11:29 +0530

    cfd

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
-export async function GET(req: NextRequest) {
-  const ip = req.headers.get("x-visitor-ip") || "Unknown IP";
-  const country = req.headers.get("x-visitor-country") || "Unknown Country";
-  const c = req.headers.get("X-Visitor") || "";
-  return NextResponse.json({ ip, country, c });
+export async function GET(request: NextRequest) {
+  // const ip = req.headers.get("x-visitor-ip") || "Unknown IP";
+  // const country = req.headers.get("x-visitor-country") || "Unknown Country";
+  // const c = request.headers.get("X-Visitor") || "";
+  const ip =
+    request.headers.get("x-real-ip") ?? request.headers.get("x-forwarded-for");
+  const city = request.headers.get("x-vercel-ip-city");
+  const country = request.headers.get("x-vercel-ip-country");
+  const region = request.headers.get("x-vercel-ip-country-region");
+  const latitude = request.headers.get("x-vercel-ip-latitude");
+  const longitude = request.headers.get("x-vercel-ip-longitude");
+  const timezone = request.headers.get("x-vercel-ip-timezone");
+  return NextResponse.json({
+    ip,
+    country,
+    city,
+    region,
+    latitude,
+    longitude,
+    timezone,
+    // c,
+  });
 }
--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,17 +1,11 @@
 // middleware.js
-import { NextRequest, NextResponse } from "next/server";
-export function middleware(req: NextRequest) {
+import { NextResponse } from "next/server";
+export function middleware() {
   // Get the user's IP address
-  const ip = req.headers.get("x-forwarded-for")?.split(",")[0] || "Unknown IP";
   // Get the user's country from Vercel's geo data
-  const country = req.geo?.country || "Unknown Country";
-  console.log(`Visitor IP: ${ip}, Country: ${country}`);
   // Create the response object
   const response = NextResponse.next();
   // Add custom headers
-  response.headers.set("X-Visitor-IP", ip);
-  response.headers.set("X-Visitor", JSON.stringify(req.geo));
-  response.headers.set("X-Visitor-Country", country);
   return response;
 }
 // Apply middleware to all routes
